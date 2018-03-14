be91395c8703dbb70ac36dafed862d7190abfe07
LiuJun <liujun2654@sina.cn>
2018-03-14 11:57:07 +0800

    'login'

--- server/appRouter.js
+++ server/appRouter.js
@@ -13,6 +13,8 @@ const md5 = require('blueimp-md5');
 const models = require('./models');
 const UserModel = models.getModel('user');
 
+const filter = {pwd:0}
+
 // 2. 得到路由器
 const router = express.Router();
 
@@ -39,11 +41,31 @@ router.post('/register',function (req,res) {
     }else {
       //不存在
       new UserModel({name,pwd:md5(pwd),type}).save(function (err,user) {
+        // 生成一个cookie(userid: user._id), 并交给浏览器保存
         res.cookie('userid',user._id);
+        // 保存成功, 返回成功的响应数据: user
         return res.send({code:0,date:{_id:user._id,name,type}})
       })
     }
   })
+});
+//登陆路由
+router.post('/login',function (req,res) {
+  // 1. 获取请求参数
+  const {name,pwd} = req.body;
+  // 2. 处理(操作数据库数据)
+  UserModel.findOne({name,pwd:md5(pwd)},filter,function (err,user) {
+    // 3. 返回响应
+    // 3.1. 如果user没有值, 返回一个错误的提示: 用户名或密码错误
+    if(!user){
+      return res.send({code:1,msg:'用户名或密码错误'})
+    }else {
+      // 生成一个cookie(userid: user._id), 并交给浏览器保存
+      res.cookie('userid',user._id);
+      return res.send({code:0,data:user})
+    }
+  })
+
 });
 
 // 4. 向外暴露路由器
--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ const appRouter = require('./appRouter');
 const app = express();
 
 
-app.use(cookieParser()) // 解析cookie数据
-app.use(bodyParser.json()) // 解析请求体(ajax请求: json数据格式)
-app.use(bodyParser.urlencoded({ extended: false })) // 解析请求体(表单数据)
+app.use(cookieParser()); // 解析cookie数据
+app.use(bodyParser.json()); // 解析请求体(ajax请求: json数据格式)
+app.use(bodyParser.urlencoded({ extended: false })); // 解析请求体(表单数据)
 // 3. 注册根路由(使用app的use())
 app.use('/api',appRouter);
 
