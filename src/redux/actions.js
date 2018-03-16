c2ecddafed7ec0116dae176f9c8949f08e1242cb
LiuJun <liujun2654@sina.cn>
2018-03-16 10:16:59 +0800

    'async/await'

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -19,16 +19,15 @@ export const register = ({name,pwd,pwd2,type}) => {
     return errorMsg('两次输入的密码不一致！');
   }
   //前台验证通过，发送异步请求
-  return dispatch => {
-    reqRegister({name,pwd,type}).then(response=>{
-      const result = response.data;
-        //判断是否成功
-      if(result.code === 0){//成功
-        dispatch(authSuccess(result.data))
-      }else {//失败
-        dispatch(errorMsg(result.msg));
-      }
-      })
+  return async dispatch => {
+    const response = await reqRegister({name,pwd,type});
+    const result = response.data;
+    //判断是否成功
+    if(result.code === 0){//成功
+      dispatch(authSuccess(result.data))
+    }else {//失败
+      dispatch(errorMsg(result.msg));
+    }
   }
 };
 
@@ -39,15 +38,14 @@ export const login = ({name,pwd}) => {
     return errorMsg('用户名或密码不能为空！')
   }
   //前台验证通过，发送异步请求
-  return dispatch => {
-    reqLogin({name,pwd}).then(response=>{
-      const result = response.data;
-      //判断是否成功
-      if(result.code === 0){//成功
-        dispatch(authSuccess(result.data));
-      }else {
-        dispatch(errorMsg(result.msg));
-      }
-    })
+  return async dispatch => {
+    const response = await reqLogin({name,pwd});
+    const result = response.data;
+    //判断是否成功
+    if(result.code === 0){//成功
+      dispatch(authSuccess(result.data));
+    }else {
+      dispatch(errorMsg(result.msg));
+    }
   }
-}
+};
